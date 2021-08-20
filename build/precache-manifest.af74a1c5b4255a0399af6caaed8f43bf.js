99ae5b84ad410d59b228e852b3b8aaaca88a0569
Sourabh Singh <51136247+sourabhsing3@users.noreply.github.com>
2021-08-20 18:38:23 +0530

    Add files via upload

---
+++ build/precache-manifest.af74a1c5b4255a0399af6caaed8f43bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7826b608177bc4e67515dd122a8a02e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de72d5f2741572651765",
+    "url": "/static/css/main.7c3777ec.chunk.css"
+  },
+  {
+    "revision": "3696c363641e190a7541",
+    "url": "/static/js/2.d54fc02e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d54fc02e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de72d5f2741572651765",
+    "url": "/static/js/main.ea46f387.chunk.js"
+  },
+  {
+    "revision": "ae594932184f88bfb438",
+    "url": "/static/js/runtime-main.9d9d98d4.js"
+  }
+]);
